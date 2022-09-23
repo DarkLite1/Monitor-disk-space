42c3b97e6d00de092ccd27b2ebd2a970445fca1c
DarkLite1 <BrechtGijbels@gmail.com>
2022-09-23 09:11:32 +0200

    update ScriptAdmin

--- Monitor disk space.ps1
+++ Monitor disk space.ps1
@@ -55,7 +55,10 @@ Param (
     [Parameter(Mandatory)]
     [String]$ImportFile,
     [String]$LogFolder = "$env:POWERSHELL_LOG_FOLDER\Monitor\Monitor disk space\$ScriptName",
-    [String[]]$ScriptAdmin = $env:POWERSHELL_SCRIPT_ADMIN
+    [String[]]$ScriptAdmin = @(
+        $env:POWERSHELL_SCRIPT_ADMIN,
+        $env:POWERSHELL_SCRIPT_ADMIN_BACKUP
+    )
 )
         
 Begin {
